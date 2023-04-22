ffcd2d1532d3c861aa80563db7a534e45fc9db10
doutimi3 <kdoutimi@gmail.com>
2023-04-22 09:40:31 +0100

    modified project9 doc and ansible automation project

--- Automate_Project_7_10/final_webserver_setup.sh
+++ Automate_Project_7_10/final_webserver_setup.sh
@@ -5,5 +5,5 @@ sudo cp -R ~/devops_tooling/html/. /var/www/html/
 sudo setenforce 0
 sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 sudo systemctl restart httpd
-sudo yum install mysql
-sudo sed -i "s/'mysql.tooling.svc.cluster.local', 'admin', 'admin'/'{{ db_private_ip }}', '{{ db_username }}', '{{ db_password }}', '{{ db_database }}'/g" /var/www/html/functions.php
+sudo yum install mysql -y
+# sudo sed -i "s/'mysql.tooling.svc.cluster.local', 'admin', 'admin'/'{{ db_private_ip }}', '{{ db_username }}', '{{ db_password }}', '{{ db_database }}'/g" /var/www/html/functions.php
